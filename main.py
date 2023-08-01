f51d7250a0a7b456a56973115afcf22cacc6c4a2
obwilson <wilsono@stu.otc.school.nz>
2023-08-01 13:22:26 +1200

    Basic tabs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk,Image 
 from tkinter import filedialog
 import shutil, atexit, os
@@ -39,15 +40,37 @@ root.title("Intellogo")
 root.iconphoto(False, icon)
 root.geometry("500x600")
 
+# Tabs
+tabs = ttk.Notebook(root)
+tabs.pack(pady=15)
+
+scale_tab = Frame(tabs, width=500, height=500, bg="Blue")
+colourblind_tab = Frame(tabs, width=500, height=500, bg="Red")
+blur_tab = Frame(tabs, width=500, height=500, bg="Blue")
+pixel_tab = Frame(tabs, width=500, height=500, bg="Red")
+bw_tab = Frame(tabs, width=500, height=500, bg="Blue")
+
+scale_tab.pack(fill="both", expand=1)
+colourblind_tab.pack(fill="both", expand=1)
+blur_tab.pack(fill="both", expand=1)
+pixel_tab.pack(fill="both", expand=1)
+bw_tab.pack(fill="both", expand=1)
+
+tabs.add(scale_tab, text="Scaled")
+tabs.add(colourblind_tab, text="Colourblind")
+tabs.add(blur_tab, text="Blurred")
+tabs.add(pixel_tab, text="Pixelated")
+tabs.add(bw_tab, text="Black/White")
+
 
 # DISPLAY
 button = Button(root, text='Choose File', command=upload_file)
 button.pack() 
 
-image = Label(root, width = 500, height = 500)
+image = Label(scale_tab, width = 500, height = 500)
 image.pack()
 
-root.after(10, main_loop)
+root.after(1, main_loop)
 root.mainloop()
 
 # EXIT MANAGER
